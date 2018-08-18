ab5fd8ce226a808101fc85052f08a73fd55c142d
Jayadeep KM <kmjayadeep@gmail.com>
2018-08-18 12:37:58 +0530

    framework added

--- framework.js
+++ framework.js
@@ -1,34 +1,45 @@
-const AVAILABLE_MODULES = require('./modules').AVAILABLE_MODULES;
-
-class FrameworkInterface {
-    constructor(moduleName) {
-        this.moduleName = moduleName;
-    }
-    triggerEvent(event) {
-        //TODO
-        console.log('event triggered', event);
-    }
-    setupBadges() {
-        //TODO
-    }
-    getEventHistory() {
-        //TODO
-    }
-}
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
 
 class Framework {
-    constructor() {
-        this.modules = {};
-        for (let moduleName of AVAILABLE_MODULES) {
-            const Module = require('./modules/' + moduleName);
-            const frameworkInterface = new FrameworkInterface(moduleName);
-            const module = new Module(frameworkInterface);
-            module.initializeModule();
-            this.modules[moduleName] = module;
-        }
+  constructor(app) {
+    this.app = app;
+    this.configureExpress(app);
+    app.use(this.route404Handler);
+    app.use(this.errorHandler);
+  }
+
+  getApp() {
+    return this.app;
+  }
+
+  configureExpress(app) {
+    app.use(logger('dev'));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({
+      extended: false
+    }));
+  }
+
+  // catch 404 and forward to error handler
+  route404Handler(req, res, next) {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  }
+
+  // error handler
+  errorHandler(err, req, res, next) {
+    // set locals, only providing error in development
+    const error = {
+      message: err.message,
+      error: req.app.get('env') === 'development' ? err : {}
     }
+    res.status(err.status || 500);
+    res.json(error);
+  }
 }
 
-//for testing
-const framework = new Framework()
-framework.modules.github.registerUser('kmjayadeep','booktradingclub','kmjayadeep');
+const app = express();
+module.exports = new Framework(app);
