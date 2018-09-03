c3873f29901a86eff6783f5608a054b6d9ea6a75
Jayadeep KM <kmjayadeep@gmail.com>
2018-09-03 07:14:38 +0530

    added protected routes

--- services/api/index.js
+++ services/api/index.js
@@ -2,14 +2,23 @@
 const router = require('express').Router();
 
 exports.useMiddleware = (url, middleware) => router.use(url, middleware);
+
 exports.addGetRoute = (url, handler) => router.get(url, handler);
 exports.addPostRoute = (url, handler) => router.post(url, handler);
-exports.addProtectedPostRoute = (url, handler) => router.post(url, (req, res, next) => {
+exports.addPutRoute = (url, handler) => router.put(url, handler);
+exports.addDeleteRoute = (url, handler) => router.delete(url, handler);
+
+const checkAuth = (req, res, next) => {
     if (req.user)
         return next();
     res.status(401).json({
         message: 'Not authorized'
     })
-}, handler);
+};
+
+exports.addProtectedGetRoute = (url, handler) => router.get(url, checkAuth, handler);
+exports.addProtectedPostRoute = (url, handler) => router.post(url, checkAuth, handler);
+exports.addProtectedPutRoute = (url, handler) => router.put(url, checkAuth, handler);
+exports.addProtectedDeleteRoute = (url, handler) => router.delete(url, checkAuth, handler);
 
 exports.router = router;
